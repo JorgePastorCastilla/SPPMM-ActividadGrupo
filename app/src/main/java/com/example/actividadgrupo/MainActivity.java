fd235014fb11fca1598ae1d7424a4ddf102237a9
Joan <jonnyy184@gmail.com>
2020-01-23 19:00:25 +0100

    Primera lista de classes

--- app/src/main/java/com/example/actividadgrupo/MainActivity.java
+++ app/src/main/java/com/example/actividadgrupo/MainActivity.java
@@ -3,16 +3,68 @@ package com.example.actividadgrupo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ArrayList<String> classes = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        classes.add("Clase 1");
+        classes.add("Clase 2");
+        displayView();
     }
 
-    private void hola() {
-        
+    private void displayView() {
+        ArrayAdapter<String> a = new ArrayAdapter<>(this, R.layout.classe, classes);
+        ListView list = findViewById(R.id.listView);
+        list.setAdapter(a);
     }
 }
