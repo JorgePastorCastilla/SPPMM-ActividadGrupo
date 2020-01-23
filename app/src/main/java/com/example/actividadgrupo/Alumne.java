a7b36ca895f9a73393b1ba2b15d1d04dea4405ad
JorgePastorCastilla <jordi131313@gmail.com>
2020-01-23 18:50:19 +0100

    creando layout para como se visualizaran los alumnos en la lista de cada clase

---
+++ app/src/main/java/com/example/actividadgrupo/Alumne.java
@@ -0,0 +1,4 @@
+package com.example.actividadgrupo;
+
+public class Alumne {
+}
---
+++ app/src/main/java/com/example/actividadgrupo/LlistaAlumnes.java
@@ -0,0 +1,14 @@
+package com.example.actividadgrupo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LlistaAlumnes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_llista_alumnes);
+    }
+}
